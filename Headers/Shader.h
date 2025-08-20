57ff86e77843e53828eba8feac4bed0901b96de7
Crosslywere <ogboruj@outlook.com>
2025-08-20 16:03:27 +0100

    Prepped shaders for files and spirv files

--- Headers/Shader.h
+++ Headers/Shader.h
@@ -3,12 +3,19 @@
 #include <unordered_map>
 #include <string>
 
+enum ShaderInputType {
+    AS_INLINE = 0,
+    AS_FILE,
+    AS_SPIRV_FILE,
+};
+
 class Shader {
 public:
-    Shader(const std::string& vs, const std::string& fs);
+    Shader(const std::string& vertex, const std::string& fragment, ShaderInputType type =  AS_INLINE);
     ~Shader();
     void use() const;
     void setFloat3(const std::string& uniform, float x, float y, float z);
+    void setTexture(const std::string& uniform, unsigned int index);
 private:
     int getUniformLocation(const std::string& name);
 private:
--- Source/Shader.cpp
+++ Source/Shader.cpp
@@ -2,20 +2,47 @@
 #include <glad/glad.h>
 #include <iostream>
 
+#define INFO_LOG_MAX_BUFFER 1024
+
+void validateShaderCompilation(unsigned int shader) {
+    int success;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        char infoLog[INFO_LOG_MAX_BUFFER];
+        glGetShaderInfoLog(shader, INFO_LOG_MAX_BUFFER, NULL, infoLog);
+        std::cerr << infoLog << std::endl;
+    }
+}
+
 unsigned int compileShaderSource(const std::string& source, int type) {
     unsigned int shader = glCreateShader(type);
-    const char* rawSource = source.c_str();
-    glShaderSource(shader, 1, &rawSource, NULL);
+    const char* sourceData = source.c_str();
+    glShaderSource(shader, 1, &sourceData, NULL);
     glCompileShader(shader);
+    validateShaderCompilation(shader);
     return shader;
 }
 
-Shader::Shader(const std::string& vs, const std::string& fs) {
+Shader::Shader(const std::string& vertex, const std::string& fragment, ShaderInputType type) {
+    unsigned int vs, fs;
+    switch (type) {
+        case AS_FILE: {
+                std::cerr << "File loading has not been implemented!" << std::endl;
+                exit(EXIT_FAILURE);
+                break;
+            }
+        case AS_SPIRV_FILE: {
+                std::cerr << "SPIR-V has not been implemented!" << std::endl;
+                exit(EXIT_FAILURE);
+                break;
+            }
+        default:
+            vs = compileShaderSource(vertex, GL_VERTEX_SHADER);
+            fs = compileShaderSource(fragment, GL_FRAGMENT_SHADER);
+        }
     m_program = glCreateProgram();
-    auto vertex = compileShaderSource(vs, GL_VERTEX_SHADER);
-    auto fragment = compileShaderSource(fs, GL_FRAGMENT_SHADER);
-    glAttachShader(m_program, vertex);
-    glAttachShader(m_program, fragment);
+    glAttachShader(m_program, vs);
+    glAttachShader(m_program, fs);
     glLinkProgram(m_program);
 }
 
@@ -29,6 +56,10 @@ void Shader::use() const {
 
 void Shader::setFloat3(const std::string& uniform, float x, float y, float z) {
     glUniform3f(getUniformLocation(uniform), x, y, z);
+}
+
+void Shader::setTexture(const std::string& uniform, unsigned int index) {
+    glUniform1i(getUniformLocation(uniform), index);
 }
 
 int Shader::getUniformLocation(const std::string& name) {
