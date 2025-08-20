fabdb4f2dea6914821b467b1476189812a48a0bd
Crosslywere <ogboruj@outlook.com>
2025-08-20 19:58:19 +0100

    Fixed texture loading

--- Source/Texture.cpp
+++ Source/Texture.cpp
@@ -7,7 +7,7 @@ Texture::Texture(const std::string& filepath, bool flipped, bool pixelated) {
     glGenTextures(1, &m_texture);
     stbi_set_flip_vertically_on_load(flipped);
     int ch;
-    auto data = stbi_load(filepath.c_str(), &m_width, &m_height, &ch, 0);
+    auto data = stbi_load((ROOT_DIR + filepath).c_str(), &m_width, &m_height, &ch, 0);
     if (!data) {
         std::cerr << stbi_failure_reason() << std::endl;
         exit(EXIT_FAILURE);
@@ -17,8 +17,21 @@ Texture::Texture(const std::string& filepath, bool flipped, bool pixelated) {
     glTextureParameteri(m_texture, GL_TEXTURE_MIN_FILTER, pixelated ? GL_NEAREST : GL_LINEAR);
     glTextureParameteri(m_texture, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTextureParameteri(m_texture, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    std::cout << ch << std::endl;
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, m_width, m_height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    int format;
+    switch (ch) {
+        case 1:
+            format = GL_RED;
+            break;
+        case 2:
+            format = GL_RG;
+            break;
+        case 3:
+            format = GL_RGB;
+            break;
+        default:
+            format = GL_RGBA;
+    }
+    glTexImage2D(GL_TEXTURE_2D, 0, format, m_width, m_height, 0, format, GL_UNSIGNED_BYTE, data);
 }
 
 Texture::~Texture() {
