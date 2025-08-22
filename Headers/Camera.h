81eed957e782c8483b7960765d976c5bafb15d94
Crosslywere <ogboruj@outlook.com>
2025-08-22 16:21:52 +0100

    Created camera class

---
+++ Headers/Camera.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+class Camera {
+    friend void updateVectors(Camera&);
+public:
+    Camera(const glm::vec3& position = glm::vec3(0, 1, -1), float pitch = 0, float yaw = -90, float fov = 90);
+    const glm::mat4 getProjection(float width, float height) const;
+    const glm::mat4 getView() const;
+    static const glm::vec3& getWorldUp();
+    const glm::vec3& getPosition() const;
+    void setPosition(const glm::vec3& position);
+    float getPitch() const;
+    void setPitch(float pitch);
+    float getYaw() const;
+    void setYaw(float yaw);
+    float getFovY() const;
+    void setFovY(float fovy);
+    const glm::vec3& getFront() const;
+    const glm::vec3& getRight() const;
+    const glm::vec3& getUp() const;
+private:
+    static const glm::vec3 WORLD_UP;
+    glm::vec3 m_position, m_front, m_right, m_up;
+    float m_pitch, m_yaw, m_fovy;
+};
---
+++ Source/Camera.cpp
@@ -0,0 +1,77 @@
+#include <Camera.h>
+#include <glm/gtc/matrix_transform.hpp>
+
+const glm::vec3 Camera::WORLD_UP = glm::vec3(0, 1, 0);
+
+Camera::Camera(const glm::vec3& position, float pitch, float yaw, float fov)
+    : m_position(position), m_pitch(pitch), m_yaw(yaw), m_fovy(fov) {
+    updateVectors(*this);
+}
+
+const glm::mat4 Camera::getProjection(float width, float height) const {
+    return glm::perspective(glm::radians(m_fovy), width / height, 0.1f, 100.f);
+}
+
+const glm::mat4 Camera::getView() const {
+    return glm::lookAt(m_position, m_position - m_front, WORLD_UP);
+}
+
+const glm::vec3& Camera::getWorldUp() {
+    return WORLD_UP;
+}
+
+const glm::vec3& Camera::getPosition() const {
+    return m_position;
+}
+
+void Camera::setPosition(const glm::vec3 &position) {
+    m_position = position;
+}
+
+float Camera::getPitch() const {
+    return m_pitch;
+}
+
+void Camera::setPitch(float pitch) {
+    m_pitch = pitch;
+    updateVectors(*this);
+}
+
+float Camera::getYaw() const {
+    return m_yaw;
+}
+
+void Camera::setYaw(float yaw) {
+    m_yaw = yaw;
+    updateVectors(*this);
+}
+
+float Camera::getFovY() const {
+    return m_fovy;
+}
+
+void Camera::setFovY(float fovy) {
+    m_fovy = fovy;
+}
+
+const glm::vec3& Camera::getFront() const {
+    return m_front;
+}
+
+const glm::vec3& Camera::getRight() const {
+    return m_right;
+}
+
+const glm::vec3& Camera::getUp() const {
+    return m_up;
+}
+
+void updateVectors(Camera& camera) {
+    glm::vec3 front{};
+    front.x = cos(glm::radians(camera.getYaw())) * cos(glm::radians(camera.getPitch()));
+    front.y = sin(glm::radians(camera.getPitch()));
+    front.z = sin(glm::radians(camera.getYaw())) * cos(glm::radians(camera.getPitch()));
+    camera.m_front = glm::normalize(front);
+    camera.m_right = glm::normalize(glm::cross(front, camera.getWorldUp()));
+    camera.m_up = glm::normalize(glm::cross(camera.getRight(), camera.getFront()));
+}
