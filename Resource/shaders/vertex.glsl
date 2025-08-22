8cd9a1fa92c72dfe83e301a9001774b3a7739763
Crosslywere <ogboruj@outlook.com>
2025-08-22 16:22:43 +0100

    Added matricies to shader for perspective projection

--- Resource/shaders/vertex.glsl
+++ Resource/shaders/vertex.glsl
@@ -1,8 +1,11 @@
 #version 330 core
 layout (location = 0) in vec2 aPos;
 layout (location = 1) in vec2 aTexCoord;
+uniform mat4 projection;
+uniform mat4 view;
+uniform mat4 model;
 varying vec2 uv;
 void main() {
     uv = aTexCoord;
-    gl_Position = vec4(aPos, 0.0, 1.0);
+    gl_Position = projection * view * model * vec4(aPos, 0.0, 1.0);
 }
--- Source/Main.cpp
+++ Source/Main.cpp
@@ -4,6 +4,8 @@
 #include <Texture.h>
 #include <iostream>
 #include <cmath>
+#include <glm/gtc/matrix_transform.hpp>
+#include <Camera.h>
 
 int main() {
     glfwInit();
@@ -26,13 +28,18 @@ int main() {
     glfwSwapInterval(1);
     // Scoped for cleanup - once the window context is destroyed, opengl calls will fail!
     {
-        // Triangle setup
+        // Rectangle setup
         float vertices[] = {
-            0.0f, 0.5f, .5f, 1.f,
+            -.5f, 0.5f, 0.f, 1.f,
             -.5f, -.5f, 0.f, 0.f,
             0.5f, -.5f, 1.f, 0.f,
+            0.5f, 0.5f, 1.f, 1.f,
         };
-        unsigned int vao, vbo;
+        unsigned int indices[] = {
+            0, 1, 2,
+            2, 3, 0,
+        };
+        unsigned int vao, vbo, ebo;
         glGenVertexArrays(1, &vao);
         glBindVertexArray(vao);
         glGenBuffers(1, &vbo);
@@ -42,18 +49,25 @@ int main() {
         glEnableVertexAttribArray(0);
         glVertexAttribPointer(1, 2, GL_FLOAT, false, 4 * sizeof(float), (const void*)(2 * sizeof(float)));
         glEnableVertexAttribArray(1);
+        glGenBuffers(1, &ebo);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
         Shader shader = Shader("Resource/shaders/vertex.glsl", "Resource/shaders/fragment.glsl", AS_FILE);
         shader.use();
+        Camera camera = Camera({0, 0, -1});
+        shader.setMat4("projection", camera.getProjection(4, 3));
+        shader.setMat4("view", camera.getView());
         Texture texture = Texture("Resource/wall.jpg");
         shader.setTexture("wall", texture.bind());
         float r, g, b;
         while (!glfwWindowShouldClose(window)) {
             glClear(GL_COLOR_BUFFER_BIT);
-            glDrawArrays(GL_TRIANGLES, 0, 3);
             r = sin(glfwGetTime() + M_PI * M_PI);
             g = sin(glfwGetTime());
             b = sin(glfwGetTime() * 2);
+            shader.setMat4("model", glm::rotate(glm::mat4(1.f), (float) glfwGetTime(), glm::vec3(0, 0, 1)));
             shader.setFloat3("color", r, g, b);
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL);
             glfwSwapBuffers(window);
             glfwPollEvents();
         }
