38f0c5fce68fb2e4d9717f3234bc4fe1adde398b
Crosslywere <ogboruj@outlook.com>
2025-09-02 13:20:57 +0100

    Updated processEvents function - Removed useless parameters -  Implemented speed augmentation - Implemented mouse camera controls

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -2,26 +2,50 @@
 #include <GraphicsEngine.h>
 #include <glm/gtc/matrix_transform.hpp>
 
-void processEvents(const Input& input, const Timer& timer, Window& window, void* other = nullptr) {
+void processEvents(Window& window, Camera& camera) {
+    static const Input& input = Input::getInstance();
+    static const Timer& timer = Timer::getInstance();
     if (input.isKeyJustPressed(GLFW_KEY_ESCAPE))
         window.quit();
-    auto camera = (Camera*)other;
+    float speed = input.isKeyPressed(GLFW_KEY_LEFT_SHIFT) ? 2.5 : 1;
     if (input.isKeyPressed(GLFW_KEY_SPACE))
-        camera->setPosition(camera->getPosition() + (camera->getWorldUp() * timer.getDeltaTime()));
+        camera.setPosition(camera.getPosition() + camera.getWorldUp() * timer.getDeltaTime() * speed);
     if (input.isKeyPressed(GLFW_KEY_LEFT_CONTROL))
-        camera->setPosition(camera->getPosition() - (camera->getWorldUp() * timer.getDeltaTime()));
+        camera.setPosition(camera.getPosition() - camera.getWorldUp() * timer.getDeltaTime() * speed);
     if (input.isKeyPressed(GLFW_KEY_W))
-        camera->setPosition(camera->getPosition() + (camera->getFront() * timer.getDeltaTime()));
+        camera.setPosition(camera.getPosition() + camera.getFront() * timer.getDeltaTime() * speed);
     if (input.isKeyPressed(GLFW_KEY_S))
-        camera->setPosition(camera->getPosition() - (camera->getFront() * timer.getDeltaTime()));
+        camera.setPosition(camera.getPosition() - camera.getFront() * timer.getDeltaTime() * speed);
     if (input.isKeyPressed(GLFW_KEY_D))
-        camera->setPosition(camera->getPosition() + (camera->getRight() * timer.getDeltaTime()));
+        camera.setPosition(camera.getPosition() + camera.getRight() * timer.getDeltaTime() * speed);
     if (input.isKeyPressed(GLFW_KEY_A))
-        camera->setPosition(camera->getPosition() - (camera->getRight() * timer.getDeltaTime()));
+        camera.setPosition(camera.getPosition() - camera.getRight() * timer.getDeltaTime() * speed);
     if (input.isKeyPressed(GLFW_KEY_E))
-        camera->setYaw(camera->getYaw() + 50 * timer.getDeltaTime());
+        camera.setYaw(camera.getYaw() + 50 * timer.getDeltaTime());
     if (input.isKeyPressed(GLFW_KEY_Q))
-        camera->setYaw(camera->getYaw() - 50 * timer.getDeltaTime());
+        camera.setYaw(camera.getYaw() - 50 * timer.getDeltaTime());
+    if (input.isKeyPressed(GLFW_KEY_L))
+        camera.setYaw(camera.getYaw() + 50 * timer.getDeltaTime());
+    if (input.isKeyPressed(GLFW_KEY_J))
+        camera.setYaw(camera.getYaw() - 50 * timer.getDeltaTime());
+    if (input.isKeyPressed(GLFW_KEY_I))
+        camera.setPitch(camera.getPitch() + 50 * timer.getDeltaTime());
+    if (input.isKeyPressed(GLFW_KEY_K))
+        camera.setPitch(camera.getPitch() - 50 * timer.getDeltaTime());
+
+    static glm::vec2 lastPos = input.getMousePosition();
+    if (input.isButtonPressed(GLFW_MOUSE_BUTTON_1)) {
+        window.disableMouse();
+        auto mousePos = input.getMousePosition();
+        float xoffset = mousePos.x - lastPos.x;
+        float yoffset = lastPos.y - mousePos.y;
+        lastPos = mousePos;
+        camera.setPitch(camera.getPitch() + yoffset * 10 * timer.getDeltaTime());
+        camera.setYaw(camera.getYaw() + xoffset * 10 * timer.getDeltaTime());
+    } else {
+        lastPos = input.getMousePosition();
+        window.normalMouse();
+    }
 }
 
 int main() {
@@ -37,7 +61,7 @@ int main() {
         Framebuffer::enableDepthTest();
         Framebuffer::setClearColor(.5, .5, .5);
         while (window.isOpen()) {
-            processEvents(Input::getInstance(), timer, window, &camera);
+            processEvents(window, camera);
             framebuffer.bind();
             {
                 Framebuffer::clear();
